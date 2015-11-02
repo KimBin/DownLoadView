a238670dae99548b2f43b08c50d87bfc84cd38a5
mac <mac@mac-3.local>
2015-11-02 19:00:12 +0800

    第一次更新

--- DownLoaView/DownLoaView/AppDelegate.m
+++ DownLoaView/DownLoaView/AppDelegate.m
@@ -13,10 +13,9 @@
 @end
 
 @implementation AppDelegate
-
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"aaaaa");
     return YES;
 }
 
